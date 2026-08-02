e4d9d9887a273522f853e7594af21b0cb04e0b36
zhixuanyap <22067284@imail.sunway.edu.my>
2026-08-02 11:49:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js 
 // !!! DO NOT commit this file to GitHub
-const GEMINI_API_KEY = 'AQ.Ab8RN6KQYKc7ceW1_ztuOmWPq4rwUZ1bp_hjsrP3-Us0yIXQUw';
+var GEMINI_API_KEY = 'AQ.Ab8RN6IhZ8_PT6sbvtrToovPFw8l7gbRIZ264Vt2E7nDrbve_A';
